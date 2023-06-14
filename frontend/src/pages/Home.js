6bb54e16b222aac04fa17a3a68a3c62e56384b12
kev0921 <kevin.hu0921@gmail.com>
2023-06-14 14:16:50 -0400

    set up data fetching from db to react app

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react'
+
+// components
+import ContactList from '../components/ContactList'
+
 const Home = () => {
+    const [contacts, setContacts] = useState(null)
+
+    useEffect(() => {
+        const fetchContacts = async () => {
+            const response = await fetch('/api/contacts')
+            const json = await response.json()
+
+            if (response.ok){
+                setContacts(json)
+            }
+        }
+
+        fetchContacts()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="contacts">
+                {contacts && contacts.map((contact) => (
+                    <ContactList key={contact._id} contact={contact}/>
+                ))}
+            </div>
         </div>
     )
 }
