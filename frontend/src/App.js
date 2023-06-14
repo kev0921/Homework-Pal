b4d1188969d275c2d0d5558773bfce434e4e88b5
kev0921 <kevin.hu0921@gmail.com>
2023-06-13 22:27:48 -0400

    setup frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="className">
+          
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
