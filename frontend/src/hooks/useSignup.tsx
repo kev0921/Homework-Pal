5447d1afe7d5d36ea0f22430c23ba339dd154c7a
kev0921 <kevin.hu0921@gmail.com>
2023-09-14 22:32:12 -0400

    created signup and login form

---
+++ frontend/src/hooks/useSignup.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { useAuthContext } from './useAuthContext'
+
+export const useSignup = () => {
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+
+    const signup = async (email: any, password: any) => {
+        setIsLoading(true)
+        setError(null)
+
+        const response = await fetch('/api/user/signup', {
+            method: 'POST',
+            headers: {'Content-Type': 'appliation/json'},
+            body: JSON.stringify({email, password})
+        })
+        const json = await response.json()
+
+        if(!response.ok) {
+            setIsLoading(false)
+            setError(json.error)
+        }
+        if (response.ok) {
+            
+        }
+    }
+}
